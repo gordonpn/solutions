4ae73bfc40983b029d419f6ed17c6a2fb1f8e027
Gordon Pham-Nguyen <gordon.pn6@gmail.com>
2020-11-25 17:07:39 -0500

    feat: add divisibility of strings problem

---
+++ divisibility-strings.py
@@ -0,0 +1,29 @@
+def divisibility_strings(plan_config: str, display_str: str):
+    if display_str not in plan_config:
+        return -1
+    times = 1
+    found = False
+    while not found:
+        test_string = times * display_str
+        if test_string == plan_config:
+            found = True
+            break
+        times += 1
+
+    curr_str = ""
+    for char in display_str:
+        curr_str += char
+        if curr_str * 2 in plan_config:
+            return len(curr_str)
+
+
+s = "wqzpuogsqcxpqizenbrhcbijieufuxgqpfijuobkqacjkdnzggijhqurwqyrnejckrnghqsyswhczwdicltjdndaebrtgcysulydcsbupkzogewkqpwtfzvjameircaloaqstsoiepynuvxmmthrsdcvrhdijgvzgmtzeijkzixtfxhrqpllspijwnsitnjazdwqzpuogsqcxpqizenbrhcbijieufuxgqpfijuobkqacjkdnzggijhqurwqyrnejckrnghqsyswhczwdicltjdndaebrtgcysulydcsbupkzogewkqpwtfzvjameircaloaqstsoiepynuvxmmthrsdcvrhdijgvzgmtzeijkzixtfxhrqpllspijwnsitnjazdwqzpuogsqcxpqizenbrhcbijieufuxgqpfijuobkqacjkdnzggijhqurwqyrnejckrnghqsyswhczwdicltjdndaebrtgcysulydcsbupkzogewkqpwtfzvjameircaloaqstsoiepynuvxmmthrsdcvrhdijgvzgmtzeijkzixtfxhrqpllspijwnsitnjazdwqzpuogsqcxpqizenbrhcbijieufuxgqpfijuobkqacjkdnzggijhqurwqyrnejckrnghqsyswhczwdicltjdndaebrtgcysulydcsbupkzogewkqpwtfzvjameircaloaqstsoiepynuvxmmthrsdcvrhdijgvzgmtzeijkzixtfxhrqpllspijwnsitnjazd"
+t = "wqzpuogsqcxpqizenbrhcbijieufuxgqpfijuobkqacjkdnzggijhqurwqyrnejckrnghqsyswhczwdicltjdndaebrtgcysulydcsbupkzogewkqpwtfzvjameircaloaqstsoiepynuvxmmthrsdcvrhdijgvzgmtzeijkzixtfxhrqpllspijwnsitnjazdwqzpuogsqcxpqizenbrhcbijieufuxgqpfijuobkqacjkdnzggijhqurwqyrnejckrnghqsyswhczwdicltjdndaebrtgcysulydcsbupkzogewkqpwtfzvjameircaloaqstsoiepynuvxmmthrsdcvrhdijgvzgmtzeijkzixtfxhrqpllspijwnsitnjazd"
+s2 = "bcdbcdbcdbcd"
+t2 = "bcdbcd"
+s3 = "asdfasdfasdfasdf"
+t3 = "bcd"
+
+print(divisibility_strings(s, t))  # 194
+print(divisibility_strings(s2, t2))  # 3
+print(divisibility_strings(s3, t3))  # -1
