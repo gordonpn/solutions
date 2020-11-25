d255204e4a8b17466542ebdcd5f32a498b6d235c
Gordon Pham-Nguyen <4203037+gordonpn@users.noreply.github.com>
2020-11-25 13:45:53 -0500

    working commit - Created using LeetHub

---
+++ slowest-key.py
@@ -0,0 +1,13 @@
+class Solution:
+    def slowestKey(self, releaseTimes: List[int], keysPressed: str) -> str:
+        pressTimes = []
+        for index in range(0, len(releaseTimes)):
+            if index == 0:
+                previousPress = releaseTimes[index]
+            else:
+                previousPress = releaseTimes[index] - releaseTimes[index - 1]
+                
+            pressTimes.append((keysPressed[index], previousPress))
+        pressTimes.sort(key=lambda x:x[0], reverse=True)
+        return max(pressTimes, key=lambda x:x[1])[0]
+​
