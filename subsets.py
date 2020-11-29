0000e59b0b1f8e3a3fd6f42e72a55cd942b6500d
Gordon Pham-Nguyen <4203037+gordonpn@users.noreply.github.com>
2020-11-29 12:47:00 -0500

    working commit - Created using LeetHub

---
+++ subsets.py
@@ -0,0 +1,8 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        results = [[]]
+        
+        for num in nums:
+            results += [i + [num] for i in results]
+            
+        return results
