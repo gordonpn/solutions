4d0e0ee2bbaecd46fea5037e2fd96326796ff680
Gordon Pham-Nguyen <gordon.pn6@gmail.com>
2020-12-01 12:59:33 -0500

    tests: add test case for transaction logs

--- transaction_logs.py
+++ transaction_logs.py
@@ -36,4 +36,16 @@ def processLogFile(logs, threshold):
 logs1 = ["88 99 200", "88 99 300", "99 32 100", "12 12 15"]
 threshold1 = 2
 
-print(processLogFile(logs1, threshold1))  # ['88', '99']
+logs2 = [
+    "345366 89921 45",
+    "029323 38239 23",
+    "38239" "345366 15",
+    "029323 38239 77",
+    "345366 38239 23",
+    "029323 345366 13",
+    "38239 38239 23",
+]
+threshold2 = 2
+
+# print(processLogFile(logs1, threshold1))  # ['88', '99']
+print(processLogFile(logs2, threshold2))  # [345366 , 38239, 029323]
