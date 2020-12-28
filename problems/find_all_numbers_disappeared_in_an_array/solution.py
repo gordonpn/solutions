d77d09e77a2cdb42500e88e2a8cc04624d6737a8
Gordon Pham-Nguyen <gordon.pn6@gmail.com>
2020-12-28 15:46:47 +0000

    Sync LeetCode submission - Find All Numbers Disappeared in an Array (python3)

---
+++ problems/find_all_numbers_disappeared_in_an_array/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        original_len = len(nums)
+        nums = set(nums)
+        results = []
+        
+        for n in range(1, original_len + 1):
+            if n not in nums:
+                results.append(n)
+                
+        return results
+                
