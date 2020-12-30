2a2d020bc4d75fcaa7f6d2b7c3e50a9081b498fd
Gordon Pham-Nguyen <gordon.pn6@gmail.com>
2020-12-30 00:05:02 +0000

    Sync LeetCode submission - Maximum Subarray (python3)

--- problems/maximum_subarray/solution.py
+++ problems/maximum_subarray/solution.py
@@ -2,12 +2,8 @@ class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         if len(nums) == 1:
             return nums[0]
-        current_start = 0
-        current_sum = nums[current_start]
-        current_max = nums[current_start]
         
-        for num in nums[1:]:
-            current_sum = max(num, current_sum + num)
-            current_max = max(current_max, current_sum)
-
-        return current_max
+        for i in range(1, len(nums)):
+            nums[i] = max(nums[i], nums[i - 1] + nums[i])
+            
+        return max(nums)
