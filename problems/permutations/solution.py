ae118c5f5c56e49f5dbfe97500a916d2fa3b9590
Gordon Pham-Nguyen <gordon.pn6@gmail.com>
2021-01-02 18:23:09 +0000

    Sync LeetCode submission - Permutations (python3)

---
+++ problems/permutations/solution.py
@@ -0,0 +1,20 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        results = []
+        
+        if len(nums) == 1:
+            return [nums[:]]
+        
+        for i in range(len(nums)):
+            n = nums.pop(0)
+            
+            permutations = self.permute(nums)
+            
+            for permutation in permutations:
+                permutation.append(n)
+            
+            results.extend(permutations)
+            
+            nums.append(n)
+            
+        return results
