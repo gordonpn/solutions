2a0e0e9d84f08243db07230a0420b6631a53c6f5
Gordon Pham-Nguyen <4203037+gordonpn@users.noreply.github.com>
2021-01-02 20:20:34 -0500

    Time: 40 ms (83.50%), Space: 14.6 MB (76.16%) - LeetHub

---
+++ search-a-2d-matrix/search-a-2d-matrix.py
@@ -0,0 +1,26 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        max_rows, max_cols = len(matrix), len(matrix[0])
+        
+        if target < matrix[0][0] or target > matrix[max_rows - 1][max_cols - 1]:
+            return False
+        
+        nums = []
+        
+        for row in range(max_rows):
+            nums.extend(matrix[row])
+        
+        start, end = 0, len(nums) - 1
+​
+        while start <= end:
+            middle = start + (end - start) // 2
+​
+            if nums[start] == target or nums[end] == target or nums[middle] == target: 
+                return True
+            if target < nums[middle]:
+                end = middle - 1
+            elif target > nums[middle]:
+                start = middle + 1
+​
+        return False
+        
