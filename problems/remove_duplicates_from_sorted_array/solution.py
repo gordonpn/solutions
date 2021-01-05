6d8d8bb70ffc26dada7cbe914fe3b7be7ed5f92c
Gordon Pham-Nguyen <gordon.pn6@gmail.com>
2021-01-05 16:20:00 +0000

    Sync LeetCode submission - Remove Duplicates from Sorted Array (python3)

--- problems/remove_duplicates_from_sorted_array/solution.py
+++ problems/remove_duplicates_from_sorted_array/solution.py
@@ -2,12 +2,13 @@ class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
         if not nums:
             return 0
+        
         index = 0
-        while True:
-            if index >= len(nums) - 1:
-                break
+        
+        while index < len(nums) - 1:
             if (nums[index] == nums[index + 1]):
                 nums.pop(index)
             else:
                 index += 1 
+                
         return len(nums)
